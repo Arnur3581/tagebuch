bffdc8b7d5dc03b47a0a2ac13abac3010e9eae2f
Arnur <arnur3581@gmail.com>
2025-10-04 17:29:15 +0500

    Methoden zum Provider hinzugefügt

--- src/contexts/NoteContext/provider.jsx
+++ src/contexts/NoteContext/provider.jsx
@@ -1,5 +1,31 @@
 import { NoteContext } from "./context";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 export const NoteContextProvider = () => {
-	return <NoteContext.Provider></NoteContext.Provider>;
+	const [notes, setNotes] = useLocalStorage("notes", []);
+
+	const addNote = (newNote) => {
+		setNotes((prev) => [
+			...prev,
+			{ id: crypto.randomUUID(), createdAt: Date.now(), ...newNote },
+		]);
+	};
+
+	const deleteNote = (id) => {
+		setNotes((prev) => prev.filter((note) => note.id !== id));
+	};
+
+	const updateNote = (updatedNote) => {
+		setNotes((prev) =>
+			prev.map((note) =>
+				note.id == updatedNote.id ? { ...note, ...updatedNote } : note
+			)
+		);
+	};
+
+	return (
+		<NoteContext.Provider
+			value={{ notes, addNote, deleteNote, updateNote }}
+		></NoteContext.Provider>
+	);
 };
